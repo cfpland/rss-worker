556be210850f3f8cd2b9333957ee7cafe4124245
Karl Hughes <khughes.me@gmail.com>
2018-05-15 13:48:42 -0500

    Adding webtask

--- readme.md
+++ readme.md
@@ -3,9 +3,12 @@ Gets conference data, saves it to JSONBin. Also provides a webtask that converts
 
 ## Running/Deploying the Collector
 - Build: `docker build -t karllhughes/cfpland-collector .`
-- Run: `docker run -e JSONBIN_STARTS_SOON_ID=... -e JSONBIN_CFP_ENDS_SOON_ID=... karllhughes/cfpland-collector:latest`
+- Run locally: `docker run -e JSONBIN_STARTS_SOON_ID=... -e JSONBIN_CFP_ENDS_SOON_ID=... karllhughes/cfpland-collector:latest`
+- Run locally with code in volume: `docker run -e JSONBIN_STARTS_SOON_ID=... -e JSONBIN_CFP_ENDS_SOON_ID=... -v $(pwd):/app karllhughes/cfpland-collector:latest`
 - Run in Hyper.sh: `hyper run -e JSONBIN_STARTS_SOON_ID=... -e JSONBIN_CFP_ENDS_SOON_ID=... karllhughes/cfpland-collector:latest`
 - Or set to run via cron: `hyper cron create --minute=0 --hour=3 --dom=* -e JSONBIN_STARTS_SOON_ID=... -e JSONBIN_CFP_ENDS_SOON_ID=... --name cfpland-collector-1 karllhughes/cfpland-collector:latest`
 
 ## Running/Deploying the Webtask
 
+- Deploy the webtask: `wt create -p node8 -n cfpland-collector -s JSONBIN_STARTS_SOON_ID=... -s JSONBIN_CFP_ENDS_SOON_ID=... index.js`
+- Two endpoints available: `/cfps`, `/starting`
--- webtask/index.js
+++ webtask/index.js
@@ -1,2 +1,59 @@
-// Converts JSON to RSS
+const app = (require('express'))();
+const fetch = require('node-fetch');
+const Webtask = require('webtask-tools');
+const RSS = require('rss');
 
+// Converts JSON to RSS
+app.get('/cfps', (req, res) => {
+  const jsonFeedUrl = 'https://api.jsonbin.io/b/' + req.webtaskContext.secrets.JSONBIN_CFP_ENDS_SOON_ID + '/latest';
+  fetch(jsonFeedUrl).then(res => res.json()).then(results => {
+    const feed = new RSS({
+      title: 'CFPs Ending Soon',
+      feed_url: jsonFeedUrl,
+      language: 'en',
+    });
+
+    for (let result of results) {
+      feed.item({
+        title: result.name,
+        description: '<p>CFPs Due: ' + result.cfpEndDate + '</p>' +
+        '<p>Location: ' + result.city + ', ' + result.country + '</p>',
+        url: result.cfpUrl || result.url,
+        date: result.cfpEndDate,
+      });
+    }
+
+    res.writeHead(200, {
+      'Content-Type': 'application/rss+xml',
+    });
+    res.end(feed.xml());
+  });
+});
+
+app.get('/starting', (req, res) => {
+  const jsonFeedUrl = 'https://api.jsonbin.io/b/' + req.webtaskContext.secrets.JSONBIN_STARTS_SOON_ID + '/latest';
+  fetch(jsonFeedUrl).then(res => res.json()).then(results => {
+    const feed = new RSS({
+      title: 'Conferences Coming Soon',
+      feed_url: jsonFeedUrl,
+      language: 'en',
+    });
+
+    for (let result of results) {
+      feed.item({
+        title: result.name,
+        description: '<p>Start Date: ' + result.startDate + '</p>' +
+        '<p>Location: ' + result.city + ', ' + result.country + '</p>',
+        url: result.url,
+        date: result.startDate,
+      });
+    }
+
+    res.writeHead(200, {
+      'Content-Type': 'application/rss+xml',
+    });
+    res.end(feed.xml());
+  });
+});
+
+module.exports = Webtask.fromExpress(app);
